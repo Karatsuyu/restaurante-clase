3a0da8a726831671985650889f3921b04076f1a3
Karatsuyu <tabjulian07@gmail.com>
2025-11-18 20:30:18 -0500

    Auto: push workspace changes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const { supabase } = require("../db.js");
+const { supabase } = require("./db.js");
 
 dotenv.config();
 
